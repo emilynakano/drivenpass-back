d4b343fac8caee56fa4d9b03cdc2cc547cdf83c5
emily <nakanodepaula@gmail.com>
2022-09-07 13:55:29 -0300

    feat: validate password

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -10,13 +10,13 @@ import * as userRepository from '../repositories/userRepository';
 import * as error from '../middlewares/errorHandlingMiddleware'
 
 export async function signUp ( dataUser:userRepository.ICreateUser ) {
-    const { password, email, name } = dataUser;
+    const { password, email } = dataUser;
 
     const passwordHash = await bcrypt.hash(password, 10);
 
     const user = await userRepository.findUser(email)
     
-    if(user) throw error.badRequest('email already exists!')
+    if(user) throw error.conflit('user')
     
     await userRepository.insertUser({...dataUser, password: passwordHash})
 }
@@ -28,13 +28,13 @@ export async function signIn( dataUser:userRepository.ILoginUser ) {
     
     if(!user) throw error.notFound('user')
     
-    const isPasswordIsValid = bcrypt.compareSync(password, user.password);
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
     
-    if(!isPasswordIsValid) throw error.unauthorized('password');
+    if(!isPasswordValid) throw error.unauthorized('password');
 
     const secretKey:string = process.env.JWT_SECRET_KEY??'secretKey';
 
-    const token = jwt.sign({ test: 'test' }, secretKey, { expiresIn: '30d' });
+    const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: '30d' });
     
     return token
 }
