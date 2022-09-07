c5a1410fc228adfaa1b3b737a9020b1307f72c95
emily <nakanodepaula@gmail.com>
2022-09-07 15:19:27 -0300

    feat: credential route with middleware schema

--- src/routes/credentialRouter.ts
+++ src/routes/credentialRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
-import * as credentialController from '../controllers/credentialController'
+import * as credentialController from '../controllers/credentialController';
+
+import schemaMiddleware from '../middlewares/schemaMiddleware';
+import credentialSchema from '../schemas/credentialSchema';
 
 const credentialRouter = Router();
 
-credentialRouter.post('/credentials', credentialController.createCredential)
+credentialRouter.post('/credentials', 
+    schemaMiddleware(credentialSchema),
+    credentialController.createCredential
+)
 
 export default credentialRouter;
--- src/schemas/credentialSchema.ts
+++ src/schemas/credentialSchema.ts
@@ -1,8 +1,10 @@
 import joi from 'joi';
 
-export const credentialSchema = joi.object({
+const credentialSchema = joi.object({
     url: joi.string().uri().required(), 
     username: joi.string().required(), 
     password: joi.string().required(), 
     title: joi.string().required().max(50)
 });
+
+export default credentialSchema;
