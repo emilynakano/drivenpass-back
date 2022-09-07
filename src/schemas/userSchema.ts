28639b2d34a0e462f66f1424328a32a33c0eed60
emily <nakanodepaula@gmail.com>
2022-09-07 06:56:52 -0300

    fix: min in schema password

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -3,7 +3,7 @@ import joi from "joi";
 export const userSignUpSchema = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string().min(10).required(),
     checkPassword: joi.string()
         .valid( joi.ref('password'))
         .required()
