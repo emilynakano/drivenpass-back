db3069b4f9ffe04d17a17695ec4a887728f34152
emily <nakanodepaula@gmail.com>
2022-09-07 18:53:37 -0300

    chore: findbytitle in credential repository

--- src/repositories/credentialRepository.ts
+++ src/repositories/credentialRepository.ts
@@ -4,6 +4,13 @@ import { credentials } from "@prisma/client";
 
 export type createCredential = Omit<credentials, "id" | "createdAt" >
 
+export async function findByTitle (title: string) {
+    return await prisma.credentials.findFirst({
+        where: {
+            title
+        }
+    })
+}
 export async function createCredential(credentialData: createCredential) {
     await prisma.credentials.create({
         data: credentialData
