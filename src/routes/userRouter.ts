12aa40384a3cd2c59a4f6361a97fe2eb679dfa7e
emily <nakanodepaula@gmail.com>
2022-09-07 11:22:43 -0300

    create route sign-in

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -12,5 +12,10 @@ userRouter.post('/sign-up',
     userController.signUp
 );
 
+userRouter.post('/sign-in', 
+    schemaMiddleware(userSchema.userSignInSchema), 
+    userController.signIn
+);
+
 
 export default userRouter;
