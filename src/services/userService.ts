5f1954409568b4188491615d2243acca1f4bc624
emily <nakanodepaula@gmail.com>
2022-09-07 07:02:20 -0300

    feat: encrypt password with bcrypt

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,7 @@
+import bcrypt from 'bcrypt';
 
 export async function signUp ( password: string, email: string, name: string ) {
+    
+    const passwordHash = await bcrypt.hash(password, 10);
     
 }
