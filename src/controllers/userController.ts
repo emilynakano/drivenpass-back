ba9d8f61fabe88f6a2bfabbe3b66e7159eb77388
emily <nakanodepaula@gmail.com>
2022-09-07 11:20:16 -0300

    create signIn controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,7 +5,15 @@ import * as userService from '../services/userService';
 export async function signUp (req: Request, res: Response) {
     const { password, email, name } = req.body;
 
-    await userService.signUp({password, email, name});
+    await userService.signUp({ password, email, name });
 
     res.status(201).send('user registred sucessfuly!')
+}
+
+export async function signIn (req: Request, res: Response) {
+    const { password, email } = req.body;
+
+    await userService.signIn({ password, email });
+
+    res.status(201).send('user logged sucessfuly!')
 }
