ff3baae491c5af26e878bcedd3c646bc0e1e81db
emily <nakanodepaula@gmail.com>
2022-09-07 13:49:29 -0300

    create error conflict

--- src/middlewares/errorHandlingMiddleware.ts
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -9,7 +9,11 @@ export function badRequest (message: string) {
 }
 
 export function unauthorized (credential: string) {
-    return { type: 'error_unauthorized', message: `invalid or non existent ${credential}`}
+    return { type: 'error_unauthorized', message: `invalid or non existent ${credential}!`}
+}
+
+export function conflit (entity: string) {
+    return { type: 'error_conflict', message: `${entity} already exists!`}
 }
 
 export default function errorHandlingMiddleware (error: any, req: Request, res: Response, next: NextFunction) {
@@ -18,6 +22,8 @@ export default function errorHandlingMiddleware (error: any, req: Request, res: 
     if (error.type === 'error_bad_request') return res.status(400).send(error.message);
 	
     if(error.type === 'error_unauthorized') return res.status(401).send(error.message);
+
+    if(error.type === 'error_conflict') return res.status(409).send(error.message);
     
     return res.status(500).send(error);
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -14,15 +14,23 @@ export async function signUp ( dataUser:userRepository.ICreateUser ) {
 
     const passwordHash = await bcrypt.hash(password, 10);
 
-    const emailExists = await userRepository.findUser(email)
+    const user = await userRepository.findUser(email)
     
-    if(emailExists) throw error.badRequest('this email already exists!')
+    if(user) throw error.badRequest('email already exists!')
     
     await userRepository.insertUser({...dataUser, password: passwordHash})
 }
 
 export async function signIn( dataUser:userRepository.ILoginUser ) {
     const { email, password } = dataUser;
+    
+    const user = await userRepository.findUser(email);
+    
+    if(!user) throw error.notFound('user')
+    
+    const isPasswordIsValid = bcrypt.compareSync(password, user.password);
+    
+    if(!isPasswordIsValid) throw error.unauthorized('password');
 
     const secretKey:string = process.env.JWT_SECRET_KEY??'secretKey';
 
