51c0074811b15eadc774ef5bb87d7a3289a5339e
emily <nakanodepaula@gmail.com>
2022-09-07 18:56:46 -0300

    feat: verify if credential title already exists

--- src/services/credentialService.ts
+++ src/services/credentialService.ts
@@ -1,5 +1,7 @@
 import * as credentialRepository from '../repositories/credentialRepository';
 
+import * as error from '../middlewares/errorHandlingMiddleware'
+
 import { encrypt, decrypt } from '../utilities/ecryptUtility';
 
 export async function createCredential (dataCredential: credentialRepository.createCredential) {
@@ -12,6 +14,9 @@ export async function createCredential (dataCredential: credentialRepository.cre
     } = dataCredential
 
     const passwordHash = encrypt(password);
+
+    const credential = await credentialRepository.findByTitle(title);
+    if(credential) throw error.conflit('title')
    
     await credentialRepository.createCredential({...dataCredential, password: passwordHash })
 }
