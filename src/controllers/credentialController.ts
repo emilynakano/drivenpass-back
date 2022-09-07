9d1a4c34565422a3f53fd5fa76d940b899c5279c
emily <nakanodepaula@gmail.com>
2022-09-07 17:02:11 -0300

    feat: add userId in datacredential

--- src/controllers/credentialController.ts
+++ src/controllers/credentialController.ts
@@ -6,7 +6,15 @@ export async function createCredential (req: Request, res: Response) {
     const { url , username, password, title } = req.body;
     const userId = res.locals.id;
 
-    await credentialService.createCredential({ url , username, password, title });
+    const dataCredential = {
+        url, 
+        username, 
+        password, 
+        title,
+        userId: Number(userId)
+    }
+
+    await credentialService.createCredential(dataCredential);
     
     res.status(201).send('credential reegistred sucessfully!')
 }
--- src/repositories/credentialRepository.ts
+++ src/repositories/credentialRepository.ts
@@ -1,3 +1,3 @@
 import { credentials } from "@prisma/client";
 
-export type createCredential = Omit<credentials, "id" | "userId" | "createdAt" >
+export type createCredential = Omit<credentials, "id" | "createdAt" >
