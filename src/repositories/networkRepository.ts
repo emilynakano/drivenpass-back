b7b577890f4523816e7497662d2ec76ac870d735
emily <nakanodepaula@gmail.com>
2022-09-08 14:35:33 -0300

    chore: network repository

---
+++ src/repositories/networkRepository.ts
@@ -0,0 +1,54 @@
+import prisma from "../databases/database";
+
+import { networks } from "@prisma/client";
+
+type createNetwork = Omit<networks, "id" | "createdAt">
+
+const selectedFields = {
+    id: true,
+    userId: false,
+    title: true,
+    netwoekName: true,
+    password: true,
+    createdAt: false
+}
+
+export async function findByTitleAndUserId (title: string, userId: number) {
+    return await prisma.networks.findFirst({
+        where: {
+            title,
+            userId
+        }
+    })
+}
+
+export async function findByUserId ( userId: number ) {
+    return await prisma.networks.findMany({
+        where: {
+            userId
+        },
+        select: selectedFields
+    })
+}
+
+export async function findByNetworkId ( networkId: number ) {
+    return await prisma.networks.findFirst({
+        where: {
+            id: networkId
+        }
+    })
+}
+
+export async function createNetwork(networkData: createNetwork) {
+    await prisma.networks.create({
+        data: networkData
+    })
+}
+
+export async function deleteNetwork(networkId: number, userId: number) {
+    await prisma.networks.delete({
+        where: {
+            id: networkId
+        }
+    })
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import userRouter from './userRouter';
 import credentialRouter from './credentialRouter';
 import securityNoteRouter from './securityNoteRouter';
+import networkRouter from './networkRouter';
 
 const router = Router();
 
 router.use(userRouter);
 router.use(credentialRouter);
 router.use(securityNoteRouter);
+router.use(networkRouter);
 
 export default router;
