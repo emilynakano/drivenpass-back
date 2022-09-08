55488770ac84ca7f286fe51322bba6527942706c
emily <nakanodepaula@gmail.com>
2022-09-08 09:18:20 -0300

    chore: create securitynote controller

--- src/controllers/securityNoteController.ts
+++ src/controllers/securityNoteController.ts
@@ -3,7 +3,18 @@ import { Request, Response } from 'express';
 import * as securityNoteService from '../services/securityNoteService'
 
 export async function createSecurityNote (req: Request, res: Response) {
+    const { note, title } = req.body;
+    const userId = res.locals.id;
 
+    const dataSecurityNote = {
+        userId: Number(userId),
+        title,
+        note,
+    }
+
+    await securityNoteService.createSecurityNote(dataSecurityNote);
+    
+    res.status(201).send('security note registred sucessfully!')
 }
 
 export async function getUserSecurityNotes (req: Request, res: Response) {
