99e06be994263a3a4be54d5e1a6c6cc05f7fe13d
emily <nakanodepaula@gmail.com>
2022-09-08 09:38:05 -0300

    chore: find by title and user id in security note repository

--- src/repositories/securityNoteRepository.ts
+++ src/repositories/securityNoteRepository.ts
@@ -1,3 +1,15 @@
+import prisma from "../databases/database";
+
 import { securityNotes } from "@prisma/client";
 
 export type createSecurityNotes= Omit<securityNotes, "id" | "createdAt">
+
+export async function findByTitleAndUserId (title: string, userId: number) {
+    return await prisma.securityNotes.findFirst({
+        where: {
+            title,
+            userId
+        }
+    })
+}
+
--- src/routes/securityNoteRouter.ts
+++ src/routes/securityNoteRouter.ts
@@ -3,11 +3,14 @@ import { Router } from 'express';
 import * as securityNoteController from '../controllers/securityNoteController';
 
 import tokenMiddleware from '../middlewares/tokenMIddleware';
+import schemaMiddleware from '../middlewares/schemaMiddleware';
+import securityNoteSchema from '../schemas/securityNoteSchema';
 
 const securityNoteRouter = Router();
 
 securityNoteRouter.post('/securityNotes', 
     tokenMiddleware,
+    schemaMiddleware(securityNoteSchema),
     securityNoteController.createSecurityNote
 );
 
