8ed2e8d5e9781a10dcb13d1fe10829ecc9811a24
emily <nakanodepaula@gmail.com>
2022-09-08 09:38:57 -0300

    chore: find by user id in security notes repository

--- src/repositories/securityNoteRepository.ts
+++ src/repositories/securityNoteRepository.ts
@@ -4,12 +4,29 @@ import { securityNotes } from "@prisma/client";
 
 export type createSecurityNotes= Omit<securityNotes, "id" | "createdAt">
 
+const selectedFields = {
+    id: true,
+    userId: false,
+    title: true,
+    note: true,
+    createdAt: false,
+}
+
 export async function findByTitleAndUserId (title: string, userId: number) {
     return await prisma.securityNotes.findFirst({
         where: {
             title,
             userId
         }
+    })
+}
+
+export async function findByUserId ( userId: number ) {
+    return await prisma.securityNotes.findMany({
+        where: {
+            userId
+        },
+        select: selectedFields
     })
 }
 
